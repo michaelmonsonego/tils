90775b99b8bc35f19b110e6146e41a7616ed9403
michael monsonego <michael12348888@gmail.com>
2024-06-23 18:05:45 +0300

    update

---
+++ cd4_data_prep_analysis.R
@@ -0,0 +1,189 @@
+
+
+library(dplyr)
+library(Seurat)
+library(patchwork)
+library(tidyverse)
+library(sleepwalk)
+library(gridExtra)
+library(future)
+library(reshape2)
+library(reticulate)
+library(ggplot2)
+library(plyr)
+library(stringr)
+library(RColorBrewer)
+library(grid)
+library(cowplot)
+library(ggsignif)
+library(DT)
+library(Cairo)
+library(snakecase)
+library(glue)
+
+setwd("D:/Michael/git_check/tils")
+# setwd("C:/Users/michael monsonego/Documents/tils") #M# update between computers(git)
+
+x1=4.6
+T_cells = readRDS("objects/tils_all_.45_integrgate_annotated.rds")
+DimPlot(T_cells, reduction = "tsne",repel = T, label = TRUE, label.size = 5)
+DefaultAssay(T_cells) <- "RNA" #M# important before subseting! otherwise doesnt find cd8
+# T_cells_0 <- subset(T_cells, idents = c("0" = "0_activated_memory_CD4")) #M# also subset out CD8 cells
+# table(T_cells_0$orig.ident)
+# table(T_cells_0$seurat_clusters)
+#M# save cd8 to move to cluster 1 cd8 analysis
+# cd8 <- subset(T_cells_0, subset = CD8A > 0.05 & CD8B > 0.05)
+# table(cd8$orig.ident)
+# table(cd8$seurat_clusters)
+# saveRDS(cd8, "objects/cd8_from_clus_0.rds")
+
+#M# clean cd8 from this analysis
+#T_cells_0 <- subset(T_cells_0, subset = CD8A < 0.005 & CD8B < 0.005)
+# table(T_cells_0$orig.ident)
+# table(T_cells_0$seurat_clusters)
+# DefaultAssay(T_cells_0)
+
+#M# check how many cells from T_cells_0 express cd4 > 0.5
+# check1 <- subset(T_cells_0, subset = CD4 > 0.5)
+# table(check1$orig.ident)
+# table(check1$seurat_clusters)
+
+
+
+#M# all cells with CD4 > 0.5 and no CD8
+cd4_high_cells <- subset(T_cells, subset = CD4 > 0.5 & CD8A < 0.005 & CD8B < 0.005)
+expression_matrix <- GetAssayData(cd4_high_cells, assay = "RNA", slot = "data")
+average_cd4_expression_all_cells <- mean(expression_matrix["CD4", ])
+# The average expression across all cells that express cd4 > 0.5
+print(average_cd4_expression_all_cells)
+
+table(cd4_high_cells$orig.ident)
+table(cd4_high_cells$seurat_clusters)
+
+
+#M# all cells with CD4 > 1 
+# cd4_high_cells <- subset(T_cells, subset = CD4 > 1 & CD8A < 0.005 & CD8B < 0.005)
+# expression_matrix <- GetAssayData(cd4_high_cells, assay = "RNA", slot = "data")
+# average_cd4_expression_all_cells <- mean(expression_matrix["CD4", ])
+# The average expression across all cells that express cd4 > 1
+# print(average_cd4_expression_all_cells)
+
+# table(cd4_high_cells$orig.ident)
+# table(cd4_high_cells$seurat_clusters)
+
+
+
+#M# this is for taking all cluster 0 cells and all cd4 high expressing cells
+# unique_cells <- unique(c(Cells(T_cells_0), Cells(cd4_high_cells)))
+# T_cells_0 <- subset(T_cells, cells = unique_cells)
+# table(T_cells_0$orig.ident)
+# table(T_cells_0$seurat_clusters)
+# DefaultAssay(T_cells_0)
+
+#M# addition of cd4 cells from other object  
+#cd4_from_1 <- readRDS("objects/cd4_from_clus_1.rds")
+#table(cd4_from_1$orig.ident)
+#table(cd4_from_1$seurat_clusters)
+#DefaultAssay(cd4_from_1) <- "RNA"
+# T_cells_0 = merge(T_cells_0, cd4_from_1) #M# this didnt work. 
+
+
+
+#M# 23.6 after conversation with Ayelet we chose this option : all cd4 high cells, without taking all cells from cluster 0 first
+T_cells_0 <- cd4_high_cells
+T_cells_0 = NormalizeData(T_cells_0, assay = "RNA",normalization.method = "LogNormalize", scale.factor = 10000) 
+T_cells_0 = FindVariableFeatures(T_cells_0, assay = "RNA", selection.method = "vst", nfeatures = 2000) 
+
+#M# for addition of cells from other object
+#cd4_from_1 = NormalizeData(cd4_from_1, assay = "RNA",normalization.method = "LogNormalize", scale.factor = 10000)  
+#cd4_from_1 = FindVariableFeatures(cd4_from_1, assay = "RNA", selection.method = "vst", nfeatures = 2000) 
+
+#features <- SelectIntegrationFeatures(object.list =list(T_cells_0, cd4_from_1))
+
+#anchors <- FindIntegrationAnchors(object.list = list(T_cells_0, cd4_from_1), anchor.features = features)
+#T_cells_0 <- IntegrateData(anchorset = anchors)
+
+#T_cells_0$Sample = factor(T_cells_0$Sample, levels = c("N1","N2","N3","N4","R1","R2","R3","R4"))
+#T_cells_0$orig.ident= factor(T_cells_0$orig.ident, levels=c("N1","N2","N3","N4","R1","R2","R3","R4"))
+
+#table(T_cells_0$orig.ident)
+#table(T_cells_0$seurat_clusters)
+#table(T_cells_0$Sample)
+#table(T_cells_0$Treatment)
+
+
+
+
+
+
+
+
+
+
+DefaultAssay(T_cells_0) <- "integrated"
+
+T_cells_0 = ScaleData(T_cells_0, verbose = FALSE)
+T_cells_0 = RunPCA(T_cells_0, npcs = 30, verbose = FALSE)
+DimPlot(T_cells_0, reduction = "pca", group.by="Treatment", cols = c("#FDCDAC", "#B3E2CD"), pt.size=1.1)
+ElbowPlot(T_cells_0, ndims = 30) + theme_classic()
+ggsave(file = "figures/data_prep_0/elbow_plot.png", dpi=300, width=10, height=10)
+
+T_cells_0 <- FindNeighbors(T_cells_0, reduction = "pca", dims = 1:14) #M# choose ?
+T_cells_0 = RunTSNE(T_cells_0, dims = 1:14)
+#M# T_cells_0 = RunUMAP(T_cells_0, dims = 1:18)
+
+res_seq <- c(.05,.3, .35, .4, .45, .5)
+for(res in res_seq){
+  T_cells_0.res_test <- FindClusters(T_cells_0, resolution = res)
+  
+  res_tSNE  <- DimPlot(T_cells_0.res_test, reduction = "tsne",
+                       repel = T, label = TRUE, label.size = 5) +
+    theme(legend.position = "none") + 
+    plot_annotation(title = paste("Res of", res))
+  assign(paste0("tSNE_",res), res_tSNE)
+}
+
+tSNE_ls <- list(tSNE_0.05,tSNE_0.3,tSNE_0.35,tSNE_0.4,tSNE_0.45,tSNE_0.5)
+all_tSNE <- plot_grid(tSNE_0.05,tSNE_0.3,tSNE_0.35,tSNE_0.4,tSNE_0.45,tSNE_0.5, ncol = 3) 
+ggsave(all_tSNE,filename = 'figures/data_prep_0/tils_0_14_dim_res_test.png', dpi=300, height=10, width=16)
+
+T_cells_0 <- FindClusters(T_cells_0, resolution = 0.3)
+saveRDS(T_cells_0, file = "objects/tils_0_.3.rds") #M# change if adjusted
+T_cells_0 <- readRDS("objects/tils_0_.3.rds")
+
+
+
+
+# Lineage identification
+# T cells
+Ts <- c('Cd4', 'Cd8a', 'Cd8b', 'Cd3d','Cd3e','Cd3g',"FOXP3")
+Ts <- toupper(Ts)
+FeaturePlot(T_cells_0, features = Ts, order=TRUE,pt.size=1, reduction="tsne", ncol=3)
+ggsave(file = "figures/data_prep_0/features_T.png", dpi=300, width=30, height=20)
+```
+Finding differentially expressed features (cluster biomarkers)
+```{r message=FALSE, warning=FALSE, cache= TRUE, fig.width=12, fig.height= 28}
+T_cells_0.markers = FindAllMarkers(T_cells_0, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25, assay = "RNA")
+
+
+Top50Markers_T =
+  T_cells_0.markers %>%
+  group_by(cluster) %>%
+  top_n(n = 50, wt = avg_log2FC) %>%
+  as.data.frame %>% 
+  arrange(cluster,-avg_log2FC)
+write_csv(Top50Markers_T, "Excels/cluster0_analysis_DE genes.csv")
+
+T_cells_0.markers %>%
+  group_by(cluster) %>%
+  top_n(n = 10, wt = avg_log2FC) -> top10_T
+DoHeatmap(T_cells_0, features = top10_T$gene) + NoLegend() + scale_fill_gradientn(colors = c("blue", "white", "red"))
+ggsave(file = "figures/data_prep_0/T cells markers - heatmap.png", dpi=300, width=12, height=20)
+
+
+
+
+
+
+
+
---
+++ cd8_data_prep.R
@@ -0,0 +1,110 @@
+
+
+library(dplyr)
+library(Seurat)
+library(patchwork)
+library(tidyverse)
+library(sleepwalk)
+library(gridExtra)
+library(future)
+library(reshape2)
+library(reticulate)
+library(ggplot2)
+library(plyr)
+library(stringr)
+library(RColorBrewer)
+library(grid)
+library(cowplot)
+library(ggsignif)
+library(DT)
+library(Cairo)
+library(snakecase)
+library(glue)
+
+setwd("D:/Michael/git_check/tils")
+# setwd("C:/Users/michael monsonego/Documents/tils") #M# update between computers(git)
+
+x1=4.6
+T_cells = readRDS("objects/tils_all_.45_integrgate_annotated.rds")
+DimPlot(T_cells, reduction = "tsne",repel = T, label = TRUE, label.size = 5)
+DefaultAssay(T_cells) <- "RNA" #M# important before subseting! otherwise doesnt find cd8
+
+#M# all cells with CD4 > 0.5 and no CD8
+cd8_high_cells <- subset(T_cells, subset = CD4 < 0.005 & (CD8A > 0.5 | CD8B < 0.5))
+expression_matrix <- GetAssayData(cd8_high_cells, assay = "RNA", slot = "data")
+average_cd4_expression_all_cells <- mean(expression_matrix["CD8A", ])
+# The average expression across all cells that express cd8A > 0.5
+print(average_cd4_expression_all_cells)
+
+table(cd8_high_cells$orig.ident)
+table(cd8_high_cells$seurat_clusters)
+
+cd8s <- cd8_high_cells
+cd8s = NormalizeData(cd8s, assay = "RNA",normalization.method = "LogNormalize", scale.factor = 10000) 
+cd8s = FindVariableFeatures(cd8s, assay = "RNA", selection.method = "vst", nfeatures = 2000) 
+
+DefaultAssay(cd8s) <- "integrated"
+
+cd8s = ScaleData(cd8s, verbose = FALSE)
+cd8s = RunPCA(cd8s, npcs = 30, verbose = FALSE)
+DimPlot(cd8s, reduction = "pca", group.by="Treatment", cols = c("#FDCDAC", "#B3E2CD"), pt.size=1.1)
+ElbowPlot(cd8s, ndims = 30) + theme_classic()
+ggsave(file = "figures/data_prep_0/elbow_plot.png", dpi=300, width=10, height=10)
+
+cd8s <- FindNeighbors(cd8s, reduction = "pca", dims = 1:14) #M# choose ?
+cd8s = RunTSNE(cd8s, dims = 1:14)
+#M# cd8s = RunUMAP(cd8s, dims = 1:18)
+
+res_seq <- c(.05,.3, .35, .4, .45, .5)
+for(res in res_seq){
+  cd8s.res_test <- FindClusters(cd8s, resolution = res)
+  
+  res_tSNE  <- DimPlot(cd8s.res_test, reduction = "tsne",
+                       repel = T, label = TRUE, label.size = 5) +
+    theme(legend.position = "none") + 
+    plot_annotation(title = paste("Res of", res))
+  assign(paste0("tSNE_",res), res_tSNE)
+}
+
+tSNE_ls <- list(tSNE_0.05,tSNE_0.3,tSNE_0.35,tSNE_0.4,tSNE_0.45,tSNE_0.5)
+all_tSNE <- plot_grid(tSNE_0.05,tSNE_0.3,tSNE_0.35,tSNE_0.4,tSNE_0.45,tSNE_0.5, ncol = 3) 
+ggsave(all_tSNE,filename = 'figures/data_prep_0/tils_0_14_dim_res_test.png', dpi=300, height=10, width=16)
+
+cd8s <- FindClusters(cd8s, resolution = 0.3)
+saveRDS(cd8s, file = "objects/tils_0_.3.rds") #M# change if adjusted
+cd8s <- readRDS("objects/tils_0_.3.rds")
+
+
+
+
+# Lineage identification
+# T cells
+Ts <- c('Cd4', 'Cd8a', 'Cd8b', 'Cd3d','Cd3e','Cd3g',"FOXP3")
+Ts <- toupper(Ts)
+FeaturePlot(cd8s, features = Ts, order=TRUE,pt.size=1, reduction="tsne", ncol=3)
+ggsave(file = "figures/data_prep_0/features_T.png", dpi=300, width=30, height=20)
+
+
+cd8s.markers = FindAllMarkers(cd8s, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25, assay = "RNA")
+
+Top50Markers_T =
+  cd8s.markers %>%
+  group_by(cluster) %>%
+  top_n(n = 50, wt = avg_log2FC) %>%
+  as.data.frame %>% 
+  arrange(cluster,-avg_log2FC)
+write_csv(Top50Markers_T, "Excels/cluster0_analysis_DE genes.csv")
+
+cd8s.markers %>%
+  group_by(cluster) %>%
+  top_n(n = 10, wt = avg_log2FC) -> top10_T
+DoHeatmap(cd8s, features = top10_T$gene) + NoLegend() + scale_fill_gradientn(colors = c("blue", "white", "red"))
+ggsave(file = "figures/data_prep_0/T cells markers - heatmap.png", dpi=300, width=12, height=20)
+
+
+
+
+
+
+
+
