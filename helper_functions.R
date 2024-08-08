42d6aa10d3d37d3a2875d871246f213b2be34e70
michaelmonsonego <142400573+michaelmonsonego@users.noreply.github.com>
2024-08-08 15:17:56 +0300

    Update helper_functions.R

--- helper_functions.R
+++ helper_functions.R
@@ -976,3 +976,231 @@ ggsave(file = "figures/Tcells/cd8_cd4_ratio.png", dpi=300, width=4, height=6)
 
 
 
+
+# cd40lg zoom in --------
+VlnPlot(cd4_cells, features = c("CD40LG"),
+        assay = "RNA", 
+        flip = TRUE, 
+        split.by = "Treatment"
+) + 
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 70, hjust = 1, size = 16, face = "bold"),
+    axis.title.x = element_blank(),
+    axis.text.y = element_text(size = 24, face = "italic"),
+    axis.title.y = element_text(size = 20, face = "bold"),
+    axis.ticks.y = element_line(size = 0.5),
+    strip.text.y = element_text(angle = 0, size = 16, face = "bold")
+  ) + 
+  geom_boxplot(alpha = 0.3, show.legend = FALSE)
+ggsave(file = "figures/cd4_cells/cd40lg_cluster_0.png", dpi=300, width=10, height=6)
+
+VlnPlot(clus1, features = c("CD40LG"),
+        assay = "RNA", 
+        flip = TRUE, 
+        split.by = "Treatment"
+) + 
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 70, hjust = 1, size = 16, face = "bold"),
+    axis.title.x = element_blank(),
+    axis.text.y = element_text(size = 24, face = "italic"),
+    axis.title.y = element_text(size = 20, face = "bold"),
+    axis.ticks.y = element_line(size = 0.5),
+    strip.text.y = element_text(angle = 0, size = 16, face = "bold")
+  ) +
+  geom_boxplot(alpha = 0.3, show.legend = FALSE)
+ggsave(file = "figures/clus1/cd40lg_cluster_1.png", dpi=300, width=10, height=6)
+
+VlnPlot(clus2, features = c("CD40LG"),
+        assay = "RNA", 
+        flip = TRUE, 
+        split.by = "Treatment"
+) + 
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 70, hjust = 1, size = 16, face = "bold"),
+    axis.title.x = element_blank(),
+    axis.text.y = element_text(size = 24, face = "italic"),
+    axis.title.y = element_text(size = 20, face = "bold"),
+    axis.ticks.y = element_line(size = 0.5),
+    strip.text.y = element_text(angle = 0, size = 16, face = "bold")
+  ) +
+  geom_boxplot(alpha = 0.3, show.legend = FALSE)
+ggsave(file = "figures/clus2/cd40lg_cluster_2.png", dpi=300, width=10, height=6)
+
+# 41bb zoom in----------
+VlnPlot(cd4_cells, features = c("TNFRSF9"),
+        assay = "RNA", 
+        flip = TRUE, 
+        split.by = "Treatment"
+) + 
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 70, hjust = 1, size = 16, face = "bold"),
+    axis.title.x = element_blank(),
+    axis.text.y = element_text(size = 24, face = "italic"),
+    axis.title.y = element_text(size = 20, face = "bold"),
+    axis.ticks.y = element_line(size = 0.5),
+    strip.text.y = element_text(angle = 0, size = 16, face = "bold")
+  ) + 
+  geom_boxplot(alpha = 0.3, show.legend = FALSE)
+ggsave(file = "figures/cd4_cells/41bb_cluster_0.png", dpi=300, width=10, height=6)
+
+
+VlnPlot(clus1, features = c("TNFRSF9"),
+        assay = "RNA", 
+        flip = TRUE, 
+        split.by = "Treatment"
+) + 
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 70, hjust = 1, size = 16, face = "bold"),
+    axis.title.x = element_blank(),
+    axis.text.y = element_text(size = 24, face = "italic"),
+    axis.title.y = element_text(size = 20, face = "bold"),
+    axis.ticks.y = element_line(size = 0.5),
+    strip.text.y = element_text(angle = 0, size = 16, face = "bold")
+  ) + 
+  geom_boxplot(alpha = 0.3, show.legend = FALSE)
+ggsave(file = "figures/clus1/41bb_cluster_1.png", dpi=300, width=10, height=6)
+
+
+VlnPlot(clus2, features = c("TNFRSF9"),
+        assay = "RNA", 
+        flip = TRUE, 
+        split.by = "Treatment"
+) + 
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 70, hjust = 1, size = 16, face = "bold"),
+    axis.title.x = element_blank(),
+    axis.text.y = element_text(size = 24, face = "italic"),
+    axis.title.y = element_text(size = 20, face = "bold"),
+    axis.ticks.y = element_line(size = 0.5),
+    strip.text.y = element_text(angle = 0, size = 16, face = "bold")
+  ) + 
+  geom_boxplot(alpha = 0.3, show.legend = FALSE)
+ggsave(file = "figures/clus2/41bb_cluster_2.png", dpi=300, width=10, height=6)
+
+# IL21 check-------------
+
+VlnPlot(cd4_cells, features = c("IL21"),
+        assay = "RNA", 
+        flip = TRUE, 
+        split.by = "Treatment"
+) + 
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 70, hjust = 1, size = 16, face = "bold"),
+    axis.title.x = element_blank(),
+    axis.text.y = element_text(size = 24, face = "italic"),
+    axis.title.y = element_text(size = 20, face = "bold"),
+    axis.ticks.y = element_line(size = 0.5),
+    strip.text.y = element_text(angle = 0, size = 16, face = "bold")
+  ) + 
+  geom_boxplot(alpha = 0.3, show.legend = FALSE)
+ggsave(file = "figures/cd4_cells/IL21_cluster_0.png", dpi=300, width=10, height=6)
+
+VlnPlot(clus1, features = c("IL21"),
+        assay = "RNA", 
+        flip = TRUE, 
+        split.by = "Treatment"
+) + 
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 70, hjust = 1, size = 16, face = "bold"),
+    axis.title.x = element_blank(),
+    axis.text.y = element_text(size = 24, face = "italic"),
+    axis.title.y = element_text(size = 20, face = "bold"),
+    axis.ticks.y = element_line(size = 0.5),
+    strip.text.y = element_text(angle = 0, size = 16, face = "bold")
+  ) + 
+  geom_boxplot(alpha = 0.3, show.legend = FALSE)
+ggsave(file = "figures/clus1/IL21_cluster_1.png", dpi=300, width=10, height=6)
+
+VlnPlot(clus2, features = c("IL21"),
+        assay = "RNA", 
+        flip = TRUE, 
+        split.by = "Treatment"
+) + 
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 70, hjust = 1, size = 16, face = "bold"),
+    axis.title.x = element_blank(),
+    axis.text.y = element_text(size = 24, face = "italic"),
+    axis.title.y = element_text(size = 20, face = "bold"),
+    axis.ticks.y = element_line(size = 0.5),
+    strip.text.y = element_text(angle = 0, size = 16, face = "bold")
+  ) + 
+  geom_boxplot(alpha = 0.3, show.legend = FALSE)
+ggsave(file = "figures/clus2/IL21_cluster_2.png", dpi=300, width=10, height=6)
+
+# checking ------------
+
+VlnPlot(cd4_cells, features = c("ZNF831", "HIVEP1"),
+        assay = "RNA", 
+        flip = TRUE, 
+        split.by = "Treatment"
+) + 
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 70, hjust = 1, size = 16, face = "bold"),
+    axis.title.x = element_blank(),
+    axis.text.y = element_text(size = 24, face = "italic"),
+    axis.title.y = element_text(size = 20, face = "bold"),
+    axis.ticks.y = element_line(size = 0.5),
+    strip.text.y = element_text(angle = 0, size = 16, face = "bold")
+  ) + 
+  geom_boxplot(alpha = 0.3, show.legend = FALSE)
+ggsave(file = "figures/cd4_cells/0.png", dpi=300, width=10, height=6)
+
+VlnPlot(clus1, features = c("ZNF831", "HIVEP1"),
+        assay = "RNA", 
+        flip = TRUE, 
+        split.by = "Treatment"
+) + 
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 70, hjust = 1, size = 16, face = "bold"),
+    axis.title.x = element_blank(),
+    axis.text.y = element_text(size = 24, face = "italic"),
+    axis.title.y = element_text(size = 20, face = "bold"),
+    axis.ticks.y = element_line(size = 0.5),
+    strip.text.y = element_text(angle = 0, size = 16, face = "bold")
+  ) + 
+  geom_boxplot(alpha = 0.3, show.legend = FALSE)
+ggsave(file = "figures/clus1/1.png", dpi=300, width=10, height=6)
+
+VlnPlot(clus2, features = c("ZNF831", "HIVEP1"),
+        assay = "RNA", 
+        flip = TRUE, 
+        split.by = "Treatment"
+) + 
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 70, hjust = 1, size = 16, face = "bold"),
+    axis.title.x = element_blank(),
+    axis.text.y = element_text(size = 24, face = "italic"),
+    axis.title.y = element_text(size = 20, face = "bold"),
+    axis.ticks.y = element_line(size = 0.5),
+    strip.text.y = element_text(angle = 0, size = 16, face = "bold")
+  ) + 
+  geom_boxplot(alpha = 0.3, show.legend = FALSE)
+ggsave(file = "figures/clus2/2.png", dpi=300, width=10, height=6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
