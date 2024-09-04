7caea27462e57dd5bcf0f8722757d73f355f14b6
michaelmonsonego <142400573+michaelmonsonego@users.noreply.github.com>
2024-09-04 12:22:12 +0300

    update

--- signatures.R
+++ signatures.R
@@ -36,13 +36,372 @@ setwd("D:/Michael/git_check/tils")
 
 
 
+# TGFb up signbarue cluster 2-------------------------------
+
+up_tgfb <- "ACVR1	APC	ARID4B	BCAR3	BMP2	BMPR1A	BMPR2	CDH1	CDK9	CDKN1C	CTNNB1	ENG	FKBP1A	FNTA	FURIN	HDAC1	HIPK2	ID1	ID2	ID3	IFNGR2	JUNB	KLF10	LEFTY2	LTBP2	MAP3K7	NCOR2	NOG	PMEPA1	PPM1A	PPP1CA	PPP1R15A	RAB31	RHOA	SERPINE1	SKI	SKIL	SLC20A1	SMAD1	SMAD3	SMAD6	SMAD7	SMURF1	SMURF2	SPTBN1	TGFB1	TGFBR1	TGIF1	THBS1	TJP1	TRIM33	UBE2D3	WWTR1	XIAP
+"
+
+up_tgfb <- strsplit(up_tgfb, "\t")[[1]]
+up_tgfb <- toupper(up_tgfb)
+up_tgfb <- trimws(up_tgfb)
+
+up_tgfb <- list(up_tgfb)
+clus2 = AddModuleScore(object = clus2, features = up_tgfb, name = "up_tgfb", assay = "RNA")
+a <- FeaturePlot(object = clus2, features = "up_tgfb1", reduction = "tsne", cols=c("grey","grey","#e46467", "#b33336", "#A73033"))+labs(title = "up_tgfb", subtitle = "up_tgfb")+ theme(plot.subtitle = element_text(hjust = 0.5))
+a
+ggsave(file = "figures/clus2/clus_2_up_tgfb.png", dpi=300, width=5, height=5)
+
+
+gglist <-  list()
+name <- "up_tgfb1"
+DefaultAssay(clus2) <- "RNA" 
+for(clus in levels(clus2$seurat_clusters)){
+  print(clus)
+  obj <- subset(clus2, subset = seurat_clusters == clus)
+  p <- SignatureScore(obj, 'up_tgfb1') + ggtitle(obj@active.ident)
+  gglist[[(as.numeric(clus)+1)]] <- p
+}
+cowplot::plot_grid(plotlist = gglist, ncol = 4, nrow = 2) + 
+  ggtitle(name)
+ggsave(file = "figures/clus2/clus_2_up_tgfb_vln.png", dpi=300, width=16, height=10)
+
+
+
+
+
+
+
+# IFNg down signbarue cluster 2-------------------------------
+down_ifng <- "Akr1c18
+Apex1
+BC094916
+Bpgm
+Capg
+Cbx3-ps6
+Ccr4
+Cnga1
+Dgcr8
+Dhx9
+Fabp5
+Fam101b
+Fgf2
+Ftsj3
+Gar1
+Gbp2
+Gm13237
+Gm13577
+Gm14085
+Gm3636
+Gm3851
+Gpatch4
+Grwd1
+Hmgb1
+Hmgn5
+Hnrnpd
+Id3
+Ifi47
+Igfbp7
+Iigp1
+Il7r
+Itgav
+Klrd1
+Mat2a
+Mlf1
+Mrpl13
+Mxd4
+Nop58
+Nrn1
+Oxct1
+Pdzk1ip1
+Pinx1
+Plekhb2
+Pno1
+Polr1b
+Prdx1
+Qtrtd1
+Rab11fip4
+Rcsd1
+RP23-77B7.1
+Rrp15
+Slc9b2
+Socs2
+Sytl2
+Thy1
+Tiam1
+Timm10
+Tma16
+Tmem38b
+Tns1
+Tsr2
+Tyms
+Utp20
+Vav2
+Xcl1
+"
+
+down_ifng <- strsplit(down_ifng, "\n")[[1]]
+down_ifng <- toupper(down_ifng)
+down_ifng <- trimws(down_ifng)
+
+down_ifng <- list(down_ifng)
+clus2 = AddModuleScore(object = clus2, features = down_ifng, name = "down_ifng", assay = "RNA")
+a <- FeaturePlot(object = clus2, features = "down_ifng1", reduction = "tsne", cols=c("grey","grey","#e46467", "#b33336", "#A73033"))+labs(title = "down_ifng", subtitle = "down_ifng")+ theme(plot.subtitle = element_text(hjust = 0.5))
+a
+ggsave(file = "figures/clus2/clus_2_down_ifng.png", dpi=300, width=5, height=5)
+
+
+gglist <-  list()
+name <- "down_ifng1"
+DefaultAssay(clus2) <- "RNA" 
+for(clus in levels(clus2$seurat_clusters)){
+  print(clus)
+  obj <- subset(clus2, subset = seurat_clusters == clus)
+  p <- SignatureScore(obj, 'down_ifng1') + ggtitle(obj@active.ident)
+  gglist[[(as.numeric(clus)+1)]] <- p
+}
+cowplot::plot_grid(plotlist = gglist, ncol = 4, nrow = 2) + 
+  ggtitle(name)
+ggsave(file = "figures/clus2/clus_2_down_ifng_vln.png", dpi=300, width=16, height=10)
+
+
+# IFNg up signbarue cluster 2-------------------------------
+
+up_ifng <- "ADAR
+APOL6
+ARID5B
+ARL4A
+AUTS2
+B2M
+BANK1
+BATF2
+BPGM
+BST2
+BTG1
+C1R
+C1S
+CASP1
+CASP3
+CASP4
+CASP7
+CASP8
+CCL2
+CCL5
+CCL7
+CD274
+CD38
+CD40
+CD69
+CD74
+CD86
+CDKN1A
+CFB
+CFH
+CIITA
+CMKLR1
+CMPK2
+CMTR1
+CSF2RB
+CXCL10
+CXCL11
+CXCL9
+DDX58
+DDX60
+DHX58
+EIF2AK2
+EIF4E3
+EPSTI1
+FAS
+FCGR1A
+FGL2
+FPR1
+GBP4
+GBP6
+GCH1
+GPR18
+GZMA
+HELZ2
+HERC6
+HIF1A
+HLA-A
+HLA-B
+HLA-DMA
+HLA-DQA1
+HLA-DRB1
+HLA-G
+ICAM1
+IDO1
+IFI27
+IFI30
+IFI35
+IFI44
+IFI44L
+IFIH1
+IFIT1
+IFIT2
+IFIT3
+IFITM2
+IFITM3
+IFNAR2
+IL10RA
+IL15
+IL15RA
+IL18BP
+IL2RB
+IL4R
+IL6
+IL7
+IRF1
+IRF2
+IRF4
+IRF5
+IRF7
+IRF8
+IRF9
+ISG15
+ISG20
+ISOC1
+ITGB7
+JAK2
+KLRK1
+LAP3
+LATS2
+LCP2
+LGALS3BP
+LY6E
+LYSMD2
+MARCHF1
+METTL7B
+MT2A
+MTHFD2
+MVP
+MX1
+MX2
+MYD88
+NAMPT
+NCOA3
+NFKB1
+NFKBIA
+NLRC5
+NMI
+NOD1
+NUP93
+OAS2
+OAS3
+OASL
+OGFR
+P2RY14
+PARP12
+PARP14
+PDE4B
+PELI1
+PFKP
+PIM1
+PLA2G4A
+PLSCR1
+PML
+PNP
+PNPT1
+PSMA2
+PSMA3
+PSMB10
+PSMB2
+PSMB8
+PSMB9
+PSME1
+PSME2
+PTGS2
+PTPN1
+PTPN2
+PTPN6
+RAPGEF6
+RBCK1
+RIPK1
+RIPK2
+RNF213
+RNF31
+RSAD2
+RTP4
+SAMD9L
+SAMHD1
+SECTM1
+SELP
+SERPING1
+SLAMF7
+SLC25A28
+SOCS1
+SOCS3
+SOD2
+SP110
+SPPL2A
+SRI
+SSPN
+ST3GAL5
+ST8SIA4
+STAT1
+STAT2
+STAT3
+STAT4
+TAP1
+TAPBP
+TDRD7
+TNFAIP2
+TNFAIP3
+TNFAIP6
+TNFSF10
+TOR1B
+TRAFD1
+TRIM14
+TRIM21
+TRIM25
+TRIM26
+TXNIP
+UBE2L6
+UPP1
+USP18
+VAMP5
+VAMP8
+VCAM1
+WARS1
+XAF1
+XCL1
+ZBP1
+ZNFX1
+"
+
+up_ifng <- strsplit(up_ifng, "\n")[[1]]
+up_ifng <- toupper(up_ifng)
+up_ifng <- trimws(up_ifng)
+
+up_ifng <- list(up_ifng)
+clus2 = AddModuleScore(object = clus2, features = up_ifng, name = "up_ifng", assay = "RNA")
+a <- FeaturePlot(object = clus2, features = "up_ifng1", reduction = "tsne", cols=c("grey","grey","#e46467", "#b33336", "#A73033"))+labs(title = "up_ifng", subtitle = "up_ifng")+ theme(plot.subtitle = element_text(hjust = 0.5))
+a
+ggsave(file = "figures/clus2/clus_2_up_ifng.png", dpi=300, width=5, height=5)
+
+
+gglist <-  list()
+name <- "up_ifng1"
+DefaultAssay(clus2) <- "RNA" 
+for(clus in levels(clus2$seurat_clusters)){
+  print(clus)
+  obj <- subset(clus2, subset = seurat_clusters == clus)
+  p <- SignatureScore(obj, 'up_ifng1') + ggtitle(obj@active.ident)
+  gglist[[(as.numeric(clus)+1)]] <- p
+}
+cowplot::plot_grid(plotlist = gglist, ncol = 4, nrow = 2) + 
+  ggtitle(name)
+ggsave(file = "figures/clus2/clus_2_up_ifng_vln.png", dpi=300, width=16, height=10)
+
+
+
+
 
 
 # telomere signature ------------------------------------------------------
 clus1 = readRDS("objects/clus1_no_cd4_second_sub_14_0.5.rds")
 clus2 = readRDS("objects/tils_2.rds")
 
-#M# downregulated in cd8-------------
+#M# downregulated in cd8 telomere signature-------------
 down_cd8 <- "ADTRP
 AIM1
 BIRC3
@@ -117,7 +476,7 @@ ggsave(file = "figures/signatures/stem_cd8_clus_2_down_vln.png", dpi=300, width=
 
 
 
-#M# upregulated in cd8 ---------
+#M# upregulated in cd8 telomere signature---------
 # DNAJA1 #M# memory cd8 DE gene for telomere legth
 
 up_cd8 <- "CORO1A
@@ -191,7 +550,7 @@ ggsave(file = "figures/signatures/stem_cd8_clus_1_up_vln.png", dpi=300, width=16
 
 
 
-#M# cd4 upregulated genes ---------
+#M# cd4 upregulated genes telomere signature ---------
 cd4_cells = readRDS("objects/tils_0_.35.rds")
 
 up_cd4_gene_list <- "CD3G
@@ -240,7 +599,7 @@ cowplot::plot_grid(plotlist = gglist, ncol = 4, nrow = 2) +
 ggsave(file = "figures/signatures/stem_cd4_up_vln.png", dpi=300, width=16, height=10)
 
 
-#down cd4 -----------
+#down cd4 telomere signature -----------
 down_cd4 <- "ARID5A
 BATF
 BAZ1A
@@ -325,40 +684,6 @@ SignatureScore <- function(object, name){
   return (p)
   
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # signatures(effector, il2..) -----------------------------------------------------------------
